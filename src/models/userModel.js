847f3de18369c9139990968cc503a70b75659bf7
Shiv Kant <shivkant639624@gmail.com>
2025-09-11 23:49:41 +0530

    working with signUpfiles

---
+++ src/models/userModel.js
@@ -0,0 +1,43 @@
+import { verify } from "crypto";
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: [true, "Username is required"],
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+      select: false, // Exclude password from queries by default
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    forgotPasswordToken: String,
+    forgotPasswordTokenExpiry: Date,
+    verifyToken: String,
+    verifyTokenExpiry: Date,
+  },
+  {
+    timestamps: true, // Automatically manage createdAt and updatedAt fields
+  }
+);
+const User = mongoose.models.users || mongoose.model("User", userSchema);
+
+export default User;
